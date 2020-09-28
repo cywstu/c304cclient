09e7b7f042fab0479915dff5377d98d38676eb5b
cywstu <46858924+cywstu@users.noreply.github.com>
2020-09-29 04:32:23 +0800

    Revert "uppercase some import references"
    This reverts commit 817c443353709e9e8aeb53f3e2208325e07c0736.

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
-import { Coffee } from "./models/Coffee";
+import { Coffee } from "./models/coffee";
 
 const httpOptions = {
   headers: new HttpHeaders({
